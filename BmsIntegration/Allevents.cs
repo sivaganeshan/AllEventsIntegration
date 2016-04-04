67e783cac2d3ca936f6c431d1cadc8c2ac0269b5
sivaganeshan <sivaganeshan36@gmail.com>
2016-04-04 18:58:42 +0530

    Base files has been added to work on
    Intimate ramachandran regarding the same.

---
+++ BmsIntegration/Allevents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace CSHttpClientSample
+{
+    static class program
+    {
+        static void simpleMain()
+        {
+            //MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        //static async void MakeRequest()
+        //{
+        //    var client = new HttpClient();
+        //    var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        //    // Request headers
+        //    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+        //    var uri = "https://api.allevents.in/geo/city_suggestion/{city}?" + queryString;
+
+        //    var response = await client.GetAsync(uri);
+        //}
+    }
+}
---
+++ BmsIntegration/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+
+namespace BmsIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string subscriptionKey = "d165d9972b2241ea85457c49bf0e4202";
+            //string outputText = string.Empty;
+            //string uriOfRequest = @"https://api.allevents.in/geo/city_suggestion/Chennai";
+            //HttpWebRequest GetTokenWebRequest = (HttpWebRequest)WebRequest.Create(uriOfRequest);
+            //GetTokenWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            //GetTokenWebRequest.ContentType = "application/json; charset=utf-8";
+            //WebResponse tokenResponse = GetTokenWebRequest.GetResponse();
+
+            //using (var sr = new StreamReader(tokenResponse.GetResponseStream()))
+            //{
+            //    outputText = sr.ReadToEnd();
+            //}
+            //TextReader tR = new TextReader
+            string jsonValueofData =  "{\"city_id\":\"13147\",\"city\":\"Chennai\",\"country_id\":\"113\",\"country\":\"India\",\"country_code\":\"IN\",\"region_id\":\"2191\",\"region\":\"Tamil Nadu\",\"region_code\":\"TN\",\"latitude\":\"13.0634000000\",\"longitude\":\"80.2207000000\",\"query\":\"Chennai\",\"timezone\":\"+05:30\"}"; 
+            
+            Dictionary<string,string> dataValue = JsonConvert.DeserializeObject<Dictionary<string,string>>(jsonValueofData);
+
+            Console.WriteLine(dataValue["city_id"]);
+
+            //JsonValue val = new JsonValue
+           // HttpWebResponse httpWebResponse = (HttpWebResponse)tokenResponse;
+
+            //Console.WriteLine(outputText);
+
+
+
+        }
+    }
+}
